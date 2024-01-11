10f5c162a890b4a5d345926d59ac595be7cc807d
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2024-01-11 15:14:13 +0100

    main: add the conversion and download KML

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,297 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from "react";
+import proj4 from "proj4";
 
-const inter = Inter({ subsets: ['latin'] })
+const convertCoordinates = (utmCoordinates, utmZone) => {
+  if (!utmZone) {
+    alert("Please select the UTM zone before converting coordinates.");
+    return null;
+  }
 
-export default function Home() {
+  const utmNordSaharaProjection = `+proj=utm +zone=${utmZone} +a=6378249.145 +rf=293.465 +towgs84=-267.407,-47.068,446.357,-0.179423,5.577661,-1.277620,1.204866 +units=m +no_defs +type=crs`;
+  proj4.defs("NordSahara", utmNordSaharaProjection);
+
+  const utmWgsProjection = `EPSG:${utmZone}`;
+  proj4.defs(
+    utmWgsProjection,
+    `+proj=utm +zone=${utmZone} +datum=WGS84 +units=m +no_defs`
+  );
+
+  // Convert UTM Nord Sahara to Geographic
+  const geographicCoordinates = proj4(
+    "NordSahara",
+    "EPSG:4326",
+    utmCoordinates
+  );
+
+  // Convert Geographic to UTM WGS84
+  const utmWgsCoordinates = proj4(
+    "EPSG:4326",
+    utmWgsProjection,
+    geographicCoordinates
+  );
+
+  return {
+    utmX: utmCoordinates[0],
+    utmY: utmCoordinates[1],
+    latitude: geographicCoordinates[1],
+    longitude: geographicCoordinates[0],
+    utmWgsX: utmWgsCoordinates[0],
+    utmWgsY: utmWgsCoordinates[1],
+  };
+};
+
+const ConverterPage = () => {
+  const [utmZone, setUtmZone] = useState(30);
+  const [fileContent, setFileContent] = useState(null);
+  const [coordinatesArray, setCoordinatesArray] = useState([]);
+  const [convertedCoordinates, setConvertedCoordinates] = useState([]);
+  const [kmlContent, setKmlContent] = useState("");
+
+  const handleFileChange = (event) => {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const content = e.target.result;
+        setFileContent(content);
+        setUtmZone(31);
+
+        const parsedCoordinatesArray = content
+          .trim()
+          .split("\n")
+          .map((line) => line.split(",").map(Number));
+
+        setCoordinatesArray(parsedCoordinatesArray);
+        setConvertedCoordinates([]);
+      };
+
+      reader.readAsText(file);
+    }
+  };
+
+  const handleCoordinateChange = (index, field, value) => {
+    const updatedCoordinates = [...coordinatesArray];
+    updatedCoordinates[index][field] = parseFloat(value); // Convert input value to float
+    setCoordinatesArray(updatedCoordinates);
+  };
+
+  const handleConvert = () => {
+    if (!utmZone) {
+      alert("Please select the UTM zone before converting coordinates.");
+      return;
+    }
+
+    const convertedCoords = coordinatesArray.map((coordinate) => {
+      return convertCoordinates(coordinate, utmZone);
+    });
+
+    setConvertedCoordinates(convertedCoords);
+
+    // Generate KML content
+    const kmlData = generateKML(convertedCoords);
+    setKmlContent(kmlData);
+  };
+
+  const generateKML = (coordinates) => {
+    let kmlString = `<?xml version="1.0" encoding="UTF-8"?>
+      <kml xmlns="http://www.opengis.net/kml/2.2">
+        <Document>
+          <name>Converted Coordinates</name>
+          <Placemark>
+            <name>Converted Coordinates</name>
+            <LineString>
+              <coordinates>`;
+
+    coordinates.forEach((coord) => {
+      kmlString += `${coord.longitude},${coord.latitude},0 `;
+    });
+
+    kmlString += `</coordinates>
+            </LineString>
+          </Placemark>
+        </Document>
+      </kml>`;
+
+    return kmlString;
+  };
+
+  const downloadKML = () => {
+    const blob = new Blob([kmlContent], {
+      type: "application/vnd.google-earth.kml+xml",
+    });
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "converted_coordinates.kml";
+    link.click();
+  };
+
+  const calculateSurfaceArea = () => {
+    if (coordinatesArray.length < 3) {
+      // A polygon must have at least 3 vertices
+      return 0;
+    }
+
+    // Add the first point to the end to close the polygon
+    const closedCoordinatesArray = [...coordinatesArray, coordinatesArray[0]];
+
+    let area = 0;
+    for (let i = 0; i < closedCoordinatesArray.length - 1; i++) {
+      const xi = closedCoordinatesArray[i][0];
+      const xiPlus1 = closedCoordinatesArray[i + 1][0];
+      const yi = closedCoordinatesArray[i][1];
+      const yiPlus1 = closedCoordinatesArray[i + 1][1];
+
+      area += xi * yiPlus1 - xiPlus1 * yi;
+    }
+
+    // Convert the area from square meters to hectares
+    const areaInHectares = Math.abs(area) / 10000;
+
+    return areaInHectares / 2;
+  };
+
+  const handleAddPoint = () => {
+    setCoordinatesArray([...coordinatesArray, [0, 0]]);
+    setConvertedCoordinates([]);
+  };
+
+  const handleDeletePoint = (index) => {
+    const updatedCoordinates = [...coordinatesArray];
+    updatedCoordinates.splice(index, 1);
+
+    const updatedConvertedCoordinates = [...convertedCoordinates];
+    updatedConvertedCoordinates.splice(index, 1);
+
+    setCoordinatesArray(updatedCoordinates);
+    setConvertedCoordinates(updatedConvertedCoordinates);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='container mx-auto p-4'>
+      <div className='mt-4 text-3xl font-bold text-center '>
+        <p>Nord SAHARA TO WGS converter (UTM) </p>
+      </div>
+      <label htmlFor='utmZone' className='block mb-2'>
+        Select UTM Zone:
+      </label>
+      <select
+        id='utmZone'
+        onChange={(e) => setUtmZone(parseInt(e.target.value))}
+        className='border border-gray-300 p-2 rounded mb-4'
+      >
+        <option value='' disabled selected>
+          Choose UTM Zone
+        </option>
+        <option value='29'>Zone 29</option>
+        <option value='30'>Zone 30</option>
+        <option value='31'>Zone 31</option>
+        <option value='32'>Zone 32</option>
+      </select>
+
+      <input
+        type='file'
+        id='fileInput'
+        accept='.csv'
+        onChange={handleFileChange}
+        className='mb-4'
+      />
+
+      <button
+        onClick={handleConvert}
+        className='bg-blue-500 text-white px-4 py-2 rounded mb-4'
+      >
+        Convert Coordinates
+      </button>
+
+      {coordinatesArray.length > 0 && (
+        <div className='mb-8'>
+          <h3 className='text-lg font-semibold mb-2'>Coordinate Table</h3>
+          <table className='table-auto w-full'>
+            <thead>
+              <tr>
+                <th className='border px-4 py-2'>UTM Nord Sahara (X)</th>
+                <th className='border px-4 py-2'>UTM Nord Sahara (Y)</th>
+                <th className='border px-4 py-2'>Geographic (Lat)</th>
+                <th className='border px-4 py-2'>Geographic (Lon)</th>
+                <th className='border px-4 py-2'>UTM WGS (X)</th>
+                <th className='border px-4 py-2'>UTM WGS (Y)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coordinatesArray.map((coordinate, index) => {
+                const convertedCoordinate = convertedCoordinates[index];
+                return (
+                  <tr key={index}>
+                    <td className='border px-4 py-2'>
+                      <input
+                        type='text'
+                        value={coordinate[0]}
+                        onChange={(e) =>
+                          handleCoordinateChange(index, 0, e.target.value)
+                        }
+                      />
+                    </td>
+                    <td className='border px-4 py-2'>
+                      <input
+                        type='text'
+                        value={coordinate[1]}
+                        onChange={(e) =>
+                          handleCoordinateChange(index, 1, e.target.value)
+                        }
+                      />
+                    </td>
+                    <td className='border px-4 py-2'>
+                      {convertedCoordinate ? convertedCoordinate.latitude : "-"}
+                    </td>
+                    <td className='border px-4 py-2'>
+                      {convertedCoordinate
+                        ? convertedCoordinate.longitude
+                        : "-"}
+                    </td>
+                    <td className='border px-4 py-2'>
+                      {convertedCoordinate ? convertedCoordinate.utmWgsX : "-"}
+                    </td>
+                    <td className='border px-4 py-2'>
+                      {convertedCoordinate ? convertedCoordinate.utmWgsY : "-"}
+                    </td>
+                    <td className='border px-4 py-2'>
+                      <button
+                        onClick={() => handleDeletePoint(index)}
+                        className='bg-red-500 text-white px-4 py-2 rounded'
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+            <button
+              onClick={handleAddPoint}
+              className='bg-gray-500 text-white px-4 py-2 rounded my-2'
+            >
+              Add Point
+            </button>
+          </table>
+
+          <button
+            onClick={downloadKML}
+            className='bg-green-500 text-white px-4 py-2 rounded my-4'
+            disabled={!kmlContent}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Download KML
+          </button>
+
+          <div className='mt-4 text-xl font-bold text-right '>
+            <p>Surface Area: {calculateSurfaceArea().toFixed(2)} hectares</p>
+          </div>
         </div>
-      </div>
+      )}
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default ConverterPage;
